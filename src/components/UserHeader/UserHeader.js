1cfdcf3be8d08f8ce12031e7437dfcb14726a553
calebpendev <calebpendev@gmail.com>
2021-09-25 11:18:29 -0700

    updated and fixed code

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -1,45 +1,51 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import {ArrowRight16} from '@carbon/icons-react';
+import { ArrowRight16 } from '@carbon/icons-react';
 
-import {HeaderContainer, Header, Image, ViewResumeLink} from './styles';
+import { HeaderContainer, Header, Image, ViewResumeLink } from './styles';
 
-const UserHeader = ({user}) => {
-    const location = useLocation();
+const UserHeader = ({ user }) => {
+  const location = useLocation();
 
-    retrun (
-        <HeaderContainer isHome={location.pathname === '/'}>\
-        <header>
-            <image src={user.basics.picture} />
-            <div>
-                <h2>{user.basics.name}</h2>
-                <h4>
-                    <a href={`https://gitconnected.com/${user.basics.username}`}
-                    target="_blank"
-                    rel="noreferrer noopener" >
-                        @{user.basics.username}
-                    </a>
-                </h4>
-                <p>{user.basics.label}</p>
-                <p>Coding in {user.basics.region}</p>
-                <p>{user.basics.yearOfExperience} years of experience as a developer</p>
-                <p>{user.basics.headline}</p>
-                <p>
-                    Blog"{''}
-                    <a href={user.basics.blog} target="_blank" rel="noreferrer noopener">{user.basics.blog}</a>
-                </p>
-            </div>
-        </header>
+  return (
+    <HeaderContainer isHome={location.pathname === '/'}>
+      <Header>
+        <Image src={user.basics.picture} />
         <div>
-            <ViewResumeLink href={`https://gitconnected.com/${user.basics.username}/resume`}
-                            target="_blank"
-                            rel="noopener noreferrer">
-                    <span>View Resume</span>
-                    <ArrowRight16/>
-            </ViewResumeLink>
+          <h2>{user.basics.name}</h2>
+          <h4>
+            <a
+              href={`https://gitconnected.com/${user.basics.username}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              @{user.basics.username}
+            </a>
+          </h4>
+          <p>{user.basics.label}</p>
+          <p>Coding in {user.basics.region}</p>
+          <p>{user.basics.yearsOfExperience} years of experience as a developer</p>
+          <p>{user.basics.headline}</p>
+          <p>
+            Blog:{' '}
+            <a href={user.basics.blog} target="_blank" rel="noreferrer noopener">
+              {user.basics.blog}
+            </a>
+          </p>
         </div>
-        </HeaderContainer>
-    );
+      </Header>
+      <div>
+        <ViewResumeLink
+          href={`https://gitconnected.com/${user.basics.username}/resume`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>View Résumé</span>
+          <ArrowRight16 />
+        </ViewResumeLink>
+      </div>
+    </HeaderContainer>
+  );
 };
 
 export default UserHeader;
